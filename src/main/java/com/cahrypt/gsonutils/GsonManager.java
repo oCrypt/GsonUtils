672a69e983f19c990e0856e1f2e2e996453e0b1d
ocrypt <universeplaysvg@gmail.com>
2023-07-05 13:02:35 -0400

    documentation

--- src/main/java/com/cahrypt/gsonutils/GsonManager.java
+++ src/main/java/com/cahrypt/gsonutils/GsonManager.java
@@ -14,24 +14,56 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
-public class GsonManager {
+/**
+ * A class that manages Gson instances and provides utility methods for serializing and deserializing objects.
+ */
+public final class GsonManager {
     private final Map<Object, Type> adapters;
+    private final Supplier<GsonBuilder> builderSupplier;
+
     private Gson gson;
 
-    public GsonManager(Gson gson) {
-        this.adapters = new HashMap<>();
-        this.gson = gson;
+    /**
+     * Creates a new GsonManager with the specified GsonBuilder supplier and caching set to true.
+     * @param builderSupplier The GsonBuilder supplier.
+     */
+    public GsonManager(Supplier<GsonBuilder> builderSupplier) {
+        this(true, builderSupplier);
     }
 
+    /**
+     * Creates a new GsonManager with a predefined GsonBuilder supplier.
+     * @param cache whether to cache the Gson instance.
+     */
     public GsonManager(boolean cache) {
+        this(cache, () -> new GsonBuilder()
+                .disableHtmlEscaping()
+                .serializeNulls()
+                .setPrettyPrinting()
+                .enableComplexMapKeySerialization()
+                .excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.STATIC)
+        );
+    }
+
+    /**
+     * Creates a new GsonManager with the specified GsonBuilder supplier.
+     * @param cache whether to cache the Gson instance.
+     * @param builderSupplier The GsonBuilder supplier.
+     */
+    public GsonManager(boolean cache, Supplier<GsonBuilder> builderSupplier) {
         this.adapters = new HashMap<>();
+        this.builderSupplier = builderSupplier;
 
         if (cache) {
             cacheGson();
         }
     }
 
+    /**
+     * Creates a new GsonManager with a predefined GsonBuilder supplier and caching set to true.
+     */
     public GsonManager() {
         this(true);
     }
@@ -63,13 +95,11 @@ public class GsonManager {
         return file;
     }
 
+    /**
+     * Caches a new Gson instance with the current adapters.
+     */
     public void cacheGson() {
-        GsonBuilder builder = new GsonBuilder()
-                .disableHtmlEscaping()
-                .serializeNulls()
-                .setPrettyPrinting()
-                .enableComplexMapKeySerialization()
-                .excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.STATIC);
+        GsonBuilder builder = builderSupplier.get();
 
         for (Map.Entry<Object, Type> entry : adapters.entrySet()) {
             builder.registerTypeAdapter(entry.getValue(), entry.getKey());
@@ -78,6 +108,13 @@ public class GsonManager {
         gson = builder.create();
     }
 
+    /**
+     * Deserializes an object from a file.
+     * @param type The type of the object.
+     * @param fileName The name of the file.
+     * @param <T> The type of the object.
+     * @return The deserialized object.
+     */
     public <T> T fromFile(Class<T> type, String fileName) {
         File file = getFile(fileName);
 
@@ -88,6 +125,13 @@ public class GsonManager {
         return fromFile(type, file);
     }
 
+    /**
+     * Deserializes an object from a file.
+     * @param type The type of the object.
+     * @param file The file.
+     * @param <T> The type of the object.
+     * @return The deserialized object.
+     */
     public <T> T fromFile(Class<T> type, File file) {
         T object = null;
 
@@ -100,6 +144,12 @@ public class GsonManager {
         return object;
     }
 
+    /**
+     * Serializes an object to a file.
+     * @param object The object.
+     * @param type The type of the object.
+     * @param fileName The name of the file to serialize to (will be created if non-existent).
+     */
     public void toFile(Object object, Type type, String fileName) {
         File file = getFile(fileName);
 
@@ -111,6 +161,12 @@ public class GsonManager {
         toFile(object, type, file);
     }
 
+    /**
+     * Serializes an object to a file.
+     * @param object The object.
+     * @param type The type of the object.
+     * @param file The file to serialize to (expected to exist).
+     */
     public void toFile(Object object, Type type, File file) {
         try {
             FileWriter writer = new FileWriter(file);
@@ -124,22 +180,45 @@ public class GsonManager {
         }
     }
 
+    /**
+     * Adds an adapter to the GsonManager.
+     * @param type The class type of the adapter.
+     * @param adapter The adapter.
+     * @param <T> The type of the adapter.
+     */
     public <T> void addAdapter(Class<T> type, JsonAdapter<T> adapter) {
         adapters.put(adapter, type);
     }
 
+    /**
+     * Adds an adapter to the GsonManager.
+     * @param type The type of the adapter.
+     * @param adapter The adapter.
+     */
     public void addAdapter(Type type, Object adapter) {
         adapters.put(adapter, type);
     }
 
+    /**
+     * Removes an adapter from the GsonManager.
+     * @param adapter The adapter.
+     */
     public void removeAdapter(Object adapter) {
         adapters.remove(adapter);
     }
 
+    /**
+     * Gets the Gson instance.
+     * @return The Gson instance.
+     */
     public Gson getGson() {
         return gson;
     }
 
+    /**
+     * Gets a snapshot of the adapters.
+     * @return The adapters.
+     */
     public Set<Object> getAdapters() {
         return new HashSet<>(adapters.keySet());
     }
